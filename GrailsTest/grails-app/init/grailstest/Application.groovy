1880e1236d6a30852b1955494a51ba383636a6fc
ferlyloic <ferlyloic@yahoo.fr>
2018-04-09 19:31:10 +0200

    modif os dalyreort

--- GrailsTest/grails-app/init/grailstest/Application.groovy
+++ GrailsTest/grails-app/init/grailstest/Application.groovy
@@ -3,8 +3,24 @@ package grailstest
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
+
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
+try {
+
+    String dbURL = "jdbc:mysql://localhost/mysql?user=root&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    Connection connection = DriverManager.getConnection(dbURL)
+    Statement statement = connection.createStatement()
+    boolean resultset = statement.execute("CREATE Database if not exists report_dev")
+    print resultset
+}catch (SQLException e){
+    e.printStackTrace()
+}
         GrailsApp.run(Application, args)
     }
 }
